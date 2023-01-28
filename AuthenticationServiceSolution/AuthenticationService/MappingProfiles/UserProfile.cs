6a8957b650b2247f7c0d410e1351322f23663164
Ted <andrei.teodor21@yahoo.ro>
2023-01-28 17:14:51 +0100

    Added some dependencies I need soon and mapping profile for Users

---
+++ AuthenticationServiceSolution/AuthenticationService/MappingProfiles/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using AuthenticationService.DTOs;
+using AuthenticationService.Models;
+using AutoMapper;
+
+namespace AuthenticationService.MappingProfiles;
+
+public class UsersProfile : Profile
+{
+    public UsersProfile()
+    {
+        // Source -> Target
+        CreateMap<User, UserDto>().ReverseMap();
+
+        CreateMap<IEnumerable<User>, IEnumerable<UserDto>>().ReverseMap();
+    }
+}
