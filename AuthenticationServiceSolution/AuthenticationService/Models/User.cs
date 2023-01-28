c7f776c05d5b03369138340b7e927fce8d0d67b4
Ted <andrei.teodor21@yahoo.ro>
2023-01-28 16:59:55 +0100

    Added User model and DTO

---
+++ AuthenticationServiceSolution/AuthenticationService/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationService.Models;
+
+public class User
+{
+    [Key]
+    public string Id { get; set; }
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    public string Password { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    public string Email { get; set; }
+}
